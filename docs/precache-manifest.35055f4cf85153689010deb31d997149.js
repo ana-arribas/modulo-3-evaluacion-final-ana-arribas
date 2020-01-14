ccc9ea19b1bff052e4b7934d55323d21c5dea147
Ana Arribas <arribas.ibd@gmail.com>
2020-01-14 17:59:20 +0100

    create docs

---
+++ docs/precache-manifest.35055f4cf85153689010deb31d997149.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be408c42830deedb9fbfe464bd0dc2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa987e83fc196533082e",
+    "url": "./static/css/main.424eef25.chunk.css"
+  },
+  {
+    "revision": "9fba9e7459c1ca4ee315",
+    "url": "./static/js/2.9f51b325.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.9f51b325.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa987e83fc196533082e",
+    "url": "./static/js/main.f00d63e5.chunk.js"
+  },
+  {
+    "revision": "1698967487f20b08d06f",
+    "url": "./static/js/runtime-main.a5100ee7.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo2.75509f2e.png"
+  }
+]);
